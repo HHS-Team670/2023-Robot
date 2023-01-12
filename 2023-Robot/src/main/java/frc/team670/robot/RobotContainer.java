d26bc6616304e9f3820c2a7c0e9a1d4c99391fb1
Ethan <ethanytchang@gmail.com>
2023-01-12 12:38:06 -0800

    added the rest of the vendordeps

---
+++ 2023-Robot/src/main/java/frc/team670/robot/RobotContainer.java
@@ -0,0 +1,76 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2018-2019 FIRST. All Rights Reserved.                        */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+package frc.team670.robot;
+
+import frc.team670.mustanglib.RobotContainerBase;
+import frc.team670.mustanglib.commands.MustangCommand;
+
+/**
+ * RobotContainer is where we put the high-level code for the robot.
+ * It contains subsystems, OI devices, etc, and has required methods
+ * (autonomousInit, periodic, etc)
+ */
+
+public class RobotContainer extends RobotContainerBase {
+
+
+    @Override
+    public void robotInit() {
+        // TODO Auto-generated method stub
+        
+    }
+
+    @Override
+    public MustangCommand getAutonomousCommand() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public void autonomousInit() {
+        // TODO Auto-generated method stub
+        
+    }
+
+    @Override
+    public void teleopInit() {
+        // TODO Auto-generated method stub
+        
+    }
+
+    @Override
+    public void testInit() {
+        // TODO Auto-generated method stub
+        
+    }
+
+    @Override
+    public void disabled() {
+        // TODO Auto-generated method stub
+        
+    }
+
+    @Override
+    public void disabledPeriodic() {
+        // TODO Auto-generated method stub
+        
+    }
+
+    @Override
+    public void periodic() {
+        // TODO Auto-generated method stub
+        
+    }
+
+    @Override
+    public void autonomousPeriodic() {
+        // TODO Auto-generated method stub
+        
+    }
+    
+}
