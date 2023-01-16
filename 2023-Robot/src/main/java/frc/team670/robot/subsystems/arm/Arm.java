95c90909456d4f4a14376693083b803f84f9827e
Aditi Kattuparambil <112968168+KattuparambilAditi@users.noreply.github.com>
2023-01-15 17:14:11 -0800

    end day 1/15

--- 2023-Robot/src/main/java/frc/team670/robot/subsystems/arm/Arm.java
+++ 2023-Robot/src/main/java/frc/team670/robot/subsystems/arm/Arm.java
@@ -42,34 +42,47 @@ public class Arm extends MustangSubsystemBase{
     public HealthState checkHealth() {
         return HealthState.GREEN;
     }
-
+//TODO: Add a get current state method
     @Override
     public void mustangPeriodic() {
         //if we're at the target, then remove from queue
-
+        
+        //targetQueue.remove(0);
+        
         //if the queue isn't empty, move towards the first item
+        if(targetQueue.size() > 0){
 
+        }
+        
         //if the queue is empty, do nothing
     }
 
-    public void addToQueue(ArmState target) {
+    public void addToQueue(ArmState start, ArmState target) {
         //use getValidPath to determine the states between the current state and the target state
-        //for every state in that list, add to queue using targetQueue.add()   
+        //for every state in that list, add to queue using targetQueue.add()  
+        for(int i = 0; i < VALID_PATHS[start.getStateID()][target.getStateID()].length; i++){
+            targetQueue.add(VALID_PATHS[start.getStateID()][target.getStateID()][i]);
+        }
+        targetQueue.add(target);
     }
     /*
      * Returns a valid list of states from the starting State to the ending State
      * This list should NOT include the starting state, but SHOULD include the ending state
      */
-    private ArmState[] getValidPath(ArmState start, ArmState finish) {
+    /*private  getValidPath(ArmState start, ArmState finish) {
         //retrieve the list of intermediate states from VALID_PATHS
+        ArrayList<ArmSt\ate> tempValidPath = new ArrayList<ArmState>();
+        tempValidPath.add(VALID_PATHS[start.getStateID()][finish.getStateID()][0]);
+        tempValidPath.add(finish);
         //Add the final ArmState to this list
         //return this list
-    }
+        return tempValidPath.toArray();//VALID_PATHS[start.getStateID()][finish.getStateID()];
+    }*/
+    
 
     @Override
     public void debugSubsystem() {
         shoulder.debugSubsystem();
         elbow.debugSubsystem();
     }
-
 }
