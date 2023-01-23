2247e61ddd8f948a15836424b64d060411657504
Ethan <ethanytchang@gmail.com>
2023-01-22 16:16:51 -0800

    added commands to code

---
+++ 2023-Robot/src/main/java/frc/team670/robot/commands/vision/AutoAlign.java
@@ -0,0 +1,36 @@
+package frc.team670.robot.commands.vision;
+
+import java.util.Map;
+import edu.wpi.first.wpilibj2.command.CommandBase;
+import frc.team670.mustanglib.commands.MustangCommand;
+import frc.team670.mustanglib.subsystems.MustangSubsystemBase;
+import frc.team670.mustanglib.subsystems.MustangSubsystemBase.HealthState;
+
+/**
+ * AutoAlign
+ */
+public class AutoAlign extends CommandBase implements MustangCommand {
+
+    @Override
+    public void initialize() {
+    }
+
+    @Override
+    public void execute() {
+    }
+
+    @Override
+    public boolean isFinished() {
+        return false;
+    }
+
+    @Override
+    public void end(boolean interrupted) {
+    }
+
+    @Override
+    public Map<MustangSubsystemBase, HealthState> getHealthRequirements() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+}
