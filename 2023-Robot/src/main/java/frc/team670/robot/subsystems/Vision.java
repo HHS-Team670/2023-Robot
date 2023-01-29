e997f29dad2d20681b6b0ed23e0bf916101a0bea
Ethan <ethanytchang@gmail.com>
2023-01-28 17:52:31 -0800

    testing move to pose relative

---
+++ 2023-Robot/src/main/java/frc/team670/robot/subsystems/Vision.java
@@ -0,0 +1,49 @@
+package frc.team670.robot.subsystems;
+
+import org.photonvision.targeting.PhotonPipelineResult;
+import org.photonvision.targeting.PhotonTrackedTarget;
+import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.math.geometry.Transform2d;
+import edu.wpi.first.math.geometry.Transform3d;
+import edu.wpi.first.math.geometry.Translation2d;
+import edu.wpi.first.wpilibj.PowerDistribution;
+import frc.team670.mustanglib.subsystems.VisionSubsystemBase;
+import frc.team670.robot.constants.RobotConstants;
+
+public class Vision extends VisionSubsystemBase {
+
+    public Vision(PowerDistribution pd) {
+        super(pd);
+        setName("Vision");
+        setCamera(RobotConstants.VISION_CAMERA_NAME);
+        super.getCamera().setDriverMode(true);
+    }
+
+    @Override
+    public void mustangPeriodic() {
+        // TODO Auto-generated method stub
+
+    }
+
+    @Override
+    public void debugSubsystem() {
+        // TODO Auto-generated method stub
+
+    }
+
+    public Transform2d getTranslationToBestTarget() {
+        PhotonPipelineResult result = camera.getLatestResult();
+        if (result.hasTargets()) {
+            PhotonTrackedTarget target = result.getBestTarget();
+            target.getSkew();
+            Transform3d cameraToTarget = target.getBestCameraToTarget();
+            cameraToTarget.getRotation();
+            double x = cameraToTarget.getX();
+            double y = cameraToTarget.getY();
+            
+            return new Transform2d(new Translation2d(x, y), new Rotation2d());
+        } else return new Transform2d();
+    }
+
+}
