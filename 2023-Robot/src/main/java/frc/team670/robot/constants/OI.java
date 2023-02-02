e8eeecfe1e03856f0df9809775c00002ffe97cb8
Hakustar15 <ax.petcu@gmail.com>
2023-02-01 16:35:47 -0800

    Minor OI Change

--- 2023-Robot/src/main/java/frc/team670/robot/constants/OI.java
+++ 2023-Robot/src/main/java/frc/team670/robot/constants/OI.java
@@ -9,7 +9,8 @@ import frc.team670.mustanglib.utils.MustangController;
 import frc.team670.mustanglib.utils.MustangController.XboxButtons;
 import frc.team670.robot.commands.drivebase.MoveToPose;
 import frc.team670.robot.subsystems.DriveBase;
-
+import frc.team670.robot.commands.arm.MoveToTarget;
+import frc.team670.robot.subsystems.arm.*;
 public class OI extends OIBase {
     // Controllers
     private static MustangController driverController = new MustangController(0);
@@ -17,7 +18,9 @@ public class OI extends OIBase {
     // Buttons
     private static JoystickButton zeroGyro = new JoystickButton(driverController, XboxButtons.X);
     private static JoystickButton move = new JoystickButton(driverController, XboxButtons.Y);
-
+    private static JoystickButton highCone = new JoystickButton(driverController, XboxButtons.B);
+    private static JoystickButton stowButton = new JoystickButton(driverController, XboxButtons.A);
+    
 
     public static MustangController getDriverController() {
         return driverController;
@@ -26,6 +29,7 @@ public class OI extends OIBase {
     @Override
     public void configureButtonBindings(MustangSubsystemBase... subsystemBases) {
         DriveBase driveBase = (DriveBase) subsystemBases[0];
+        Arm arm = (Arm) subsystemBases[1];
 
         driveBase.initDefaultCommand();
 
@@ -33,6 +37,8 @@ public class OI extends OIBase {
                                                                    // Button.whenPressed(), used
                                                                    // Trigger.onTrue()
         move.onTrue(new MoveToPose(driveBase, 1, 1, false));
+        highCone.onTrue(new MoveToTarget(ArmState.HIGH_SHELF, arm));
+        stowButton.onTrue(new MoveToTarget(ArmState.HOPPER, arm));
     }
 
 }
