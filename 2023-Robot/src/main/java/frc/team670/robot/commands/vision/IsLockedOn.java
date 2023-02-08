478b99c0c543d4c6fe6cf5d09f0923ea9fd9554c
DevTeamHRT <dev@homesteadrobotics.com>
2023-02-07 18:07:43 -0800

    IsLockedOn command - not tested
    Co-Authored-By: Tarini Maram <56371094+tarinimaram@users.noreply.github.com>

--- 2023-Robot/src/main/java/frc/team670/robot/commands/vision/IsLockedOn.java
+++ 2023-Robot/src/main/java/frc/team670/robot/commands/vision/IsLockedOn.java
@@ -11,6 +11,16 @@ import frc.team670.mustanglib.subsystems.MustangSubsystemBase.HealthState;
  */
 public class IsLockedOn extends CommandBase implements MustangCommand {
 
+    private SwerveDrive driveBase;
+    private VisionSubsystemBase vision;
+    private Pose2d currentPose;
+    private Pose2d goalPose;
+
+    public IsLockedOn(SwerveDrive driveBase, VisionSubsystemBase vision) {
+        this.driveBase = driveBase;
+        this.vision = vision;
+    }
+
     @Override
     public Map<MustangSubsystemBase, HealthState> getHealthRequirements() {
         // TODO Auto-generated method stub
@@ -19,6 +29,22 @@ public class IsLockedOn extends CommandBase implements MustangCommand {
 
     @Override
     public void initialize() {
+        // find pose of nearest target
+        currentPose = swerve.getPose();
+        var result = vision.getCamera().getLatestResult();
+        if (!result.hasTargets()) return;
+
+        var camToTarget = result.getBestTarget().getBestCameraToTarget();
+        Transform2d transform = new Transform2d(camToTarget.getTranslation().toTranslation2d(),
+                camToTarget.getRotation().toRotation2d());
+        Pose2d cameraPose = robotPose.transformBy(RobotConstants.CAMERA_OFFSET.inverse());
+        Pose2d targetPose = cameraPose.transformBy(transform);
+
+        // transform by offset (to not crash)
+        goalPose = targetPose.transformBy(RobotConstants.GRID_TO_TARGET_OFFSET);
+        // SmartDashboard.putNumber("goal pose x", goalPose.getX());
+        // SmartDashboard.putNumber("goal pose y", goalPose.getY());
+
     }
 
     @Override
@@ -27,11 +53,15 @@ public class IsLockedOn extends CommandBase implements MustangCommand {
 
     @Override
     public boolean isFinished() {
+        if (Math.abs(currentPose.getX() - goalPose.getX()) <= 0.3 && Math.abs(currentPose.getY() - goalPose.getY()) <= 0.3 && Math.abs(currentPose.getRotation().toRotation2d().toDegrees() - goalPose.getRotation().toRotation2d().toDegrees()) <= 4) {
+            return true;
+        }
         return false;
     }
 
     @Override
     public void end(boolean interrupted) {
+        vision.switchLEDS(true, true);
     }
     
 }
