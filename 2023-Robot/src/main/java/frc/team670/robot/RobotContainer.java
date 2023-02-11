e9b6a2ff49855e2ed0417ac4de8fefdbf1ed49da
Ethan <ethanytchang@gmail.com>
2023-02-11 14:02:16 -0800

    minor changes

--- 2023-Robot/src/main/java/frc/team670/robot/RobotContainer.java
+++ 2023-Robot/src/main/java/frc/team670/robot/RobotContainer.java
@@ -123,6 +123,8 @@ public class RobotContainer extends RobotContainerBase {
         return OI.getDriverController();
     }
 
+    
+
     public MustangController getBackupController() {
         // TODO Auto-generated method stub
         return null;
