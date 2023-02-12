85de3523e3d9fe04899d61b3f93b309044eb62df
Ethan <ethanytchang@gmail.com>
2023-02-12 15:58:28 -0800

    poseestimator changes

--- 2023-Robot/src/main/java/frc/team670/robot/RobotContainer.java
+++ 2023-Robot/src/main/java/frc/team670/robot/RobotContainer.java
@@ -36,10 +36,10 @@ public class RobotContainer extends RobotContainerBase {
 
     private final PowerDistribution pd = new PowerDistribution(1, ModuleType.kCTRE);
     
-    private final DriveBase driveBase = new DriveBase(getDriverController());
     private final Vision vision = new Vision(pd);
     private final Arm arm = new Arm();
-    private final Claw claw = new Claw();
+    private final Claw claw = new Claw();    private final DriveBase driveBase = new DriveBase(getDriverController(), vision);
+
     private static OI oi = new OI();
     
     private Notifier updateArbitraryFeedForwards;
--- 2023-Robot/src/main/java/frc/team670/robot/constants/FieldConstants.java
+++ 2023-Robot/src/main/java/frc/team670/robot/constants/FieldConstants.java
@@ -1,7 +1,10 @@
 package frc.team670.robot.constants;
 
-public class FieldConstants {
-    public static final double length = 10;
-    public static final double width = 10;
-    
+import edu.wpi.first.apriltag.AprilTagFieldLayout;
+
+public class FieldConstants {    // TODO: set field layout
+    public static final double LENGTH = 10; 
+    public static final double WIDTH = 10;
+    public static final AprilTagFieldLayout VISION_FIELD_LAYOUT =
+            new AprilTagFieldLayout(null, LENGTH, WIDTH);  
 }
