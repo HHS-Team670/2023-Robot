63dd97fff4a7d317a2be4b1edc9b7be0c8eb56f3
arghunter <acgo8888@gmail.com>
2023-02-15 17:17:01 -0800

    claw idle

--- 2023-Robot/src/main/java/frc/team670/robot/constants/OI.java
+++ 2023-Robot/src/main/java/frc/team670/robot/constants/OI.java
@@ -18,6 +18,7 @@ import frc.team670.robot.subsystems.Vision;
 import frc.team670.robot.commands.arm.MoveToTarget;
 import frc.team670.robot.commands.claw.ClawEject;
 import frc.team670.robot.commands.claw.ClawIntake;
+import frc.team670.robot.commands.claw.ClawIdle;
 import frc.team670.robot.subsystems.arm.Arm;
 import frc.team670.robot.subsystems.arm.ArmState;
 
@@ -30,20 +31,19 @@ public class OI extends OIBase {
     private static JoystickButton zeroGyro = new JoystickButton(driverController, XboxButtons.X);
     private static JoystickButton moveToTarget = new JoystickButton(driverController, XboxButtons.Y);
 
-    //private static JoystickButton move = new JoystickButton(driverController, XboxButtons.Y);
+    // private static JoystickButton move = new JoystickButton(driverController,
+    // XboxButtons.Y);
 
     // Operator buttons
     private static POVButton backward = new POVButton(operatorController, 180);
-    private static POVButton scoreMid = new POVButton(operatorController, 90);
-    private static POVButton scoreMid = new POVButton(operatorController, 270);
+    private static POVButton scoreMidR = new POVButton(operatorController, 90);
+    private static POVButton scoreMidL = new POVButton(operatorController, 270);
     private static POVButton scoreHigh = new POVButton(operatorController, 0);
     private static JoystickButton stow = new JoystickButton(operatorController, XboxButtons.B);
 
     private static JoystickButton clawSuck = new JoystickButton(operatorController, XboxButtons.RIGHT_BUMPER);
     private static JoystickButton clawEject = new JoystickButton(operatorController, XboxButtons.LEFT_BUMPER);
-
-
-
+    private static JoystickButton clawIdle = new JoystickButton(operatorController, XboxButtons.Y);
 
     public static MustangController getDriverController() {
         return driverController;
@@ -66,22 +66,25 @@ public class OI extends OIBase {
                                                                    // Button.whenPressed(), used
                                                                    // Trigger.onTrue()
         moveToTarget.onTrue(new AutoAlign(vision, driveBase));
-        // move.onTrue(new MoveToPose(driveBase, new Pose2d(1, 1, new Rotation2d()), true));
+        // move.onTrue(new MoveToPose(driveBase, new Pose2d(1, 1, new Rotation2d()),
+        // true));
 
         // //arm movement commands
         backward.onTrue(scheduleMoveToTarget(arm, ArmState.BACKWARD_GROUND));
-        scoreMid.onTrue(scheduleMoveToTarget(arm, ArmState.SCORE_MID));
+        scoreMidR.onTrue(scheduleMoveToTarget(arm, ArmState.SCORE_MID));
+        scoreMidL.onTrue(scheduleMoveToTarget(arm, ArmState.SCORE_MID));
         scoreHigh.onTrue(scheduleMoveToTarget(arm, ArmState.SCORE_HIGH));
-        stow.onTrue(scheduleMoveToTarget(arm,ArmState.STOWED));
-        
+        stow.onTrue(scheduleMoveToTarget(arm, ArmState.STOWED));
 
-        //Claw control commands
+        // Claw control commands
         clawSuck.onTrue(new ClawIntake(claw));
         clawEject.onTrue(new ClawEject(claw));
-        
+        clawIdle.onTrue(new ClawIdle(claw));
+
     }
-    private InstantCommand scheduleMoveToTarget(Arm arm, ArmState target){
-        return new InstantCommand(){
+
+    private InstantCommand scheduleMoveToTarget(Arm arm, ArmState target) {
+        return new InstantCommand() {
             public void initialize() {
                 MustangScheduler.getInstance().schedule(new MoveToTarget(arm, target));
             }
