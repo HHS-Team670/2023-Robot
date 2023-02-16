ce03692aec5d71e2ee4145371606beabe968904f
Ethan <ethanytchang@gmail.com>
2023-02-15 22:13:35 -0800

    implemented photon pose estimation

--- 2023-Robot/src/main/java/frc/team670/robot/subsystems/Vision.java
+++ 2023-Robot/src/main/java/frc/team670/robot/subsystems/Vision.java
@@ -3,6 +3,7 @@ package frc.team670.robot.subsystems;
 import org.photonvision.PhotonCamera;
 import edu.wpi.first.apriltag.AprilTagFieldLayout;
 import edu.wpi.first.math.Pair;
+import edu.wpi.first.math.geometry.Transform3d;
 import edu.wpi.first.wpilibj.PowerDistribution;
 import frc.team670.mustanglib.subsystems.VisionSubsystemBase;
 import frc.team670.robot.constants.FieldConstants;
@@ -11,21 +12,16 @@ import frc.team670.robot.constants.RobotConstants;
 public class Vision extends VisionSubsystemBase {
 
     public Vision(PowerDistribution pd) {
-        super(pd, FieldConstants.VISION_FIELD_LAYOUT, new Pair<>(
-                new PhotonCamera(RobotConstants.VISION_CAMERA_NAME), RobotConstants.CAMERA_OFFSET));
+        super(pd, FieldConstants.VISION_FIELD_LAYOUT,
+                new PhotonCamera[] {new PhotonCamera(RobotConstants.VISION_CAMERA_NAME)},
+                new Transform3d[] {RobotConstants.CAMERA_OFFSET});
         setName("Vision");
         getCameras()[0].setDriverMode(true);
     }
 
     @Override
-    public void mustangPeriodic() {
-        // TODO Auto-generated method stub
-        // super.processImage(0, 0, 0); //TODO: find camera offsets
-    }
+    public void mustangPeriodic() {}
 
     @Override
-    public void debugSubsystem() {
-        // TODO Auto-generated method stub
-
-    }
+    public void debugSubsystem() {}
 }
