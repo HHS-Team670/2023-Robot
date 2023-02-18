3656a32a7202f6ddf683985ad2f721709e92d5bd
Ethan <ethanytchang@gmail.com>
2023-02-18 15:58:30 -0800

    added test bench entries

--- 2023-Robot/src/main/java/frc/team670/robot/constants/RobotConstants.java
+++ 2023-Robot/src/main/java/frc/team670/robot/constants/RobotConstants.java
@@ -66,6 +66,12 @@ public final class RobotConstants extends RobotConstantsBase {
                     entry("ELBOW_ABSOLUTE_ENCODER_AT_VERTICAL", 0.944))),
             entry("00:80:2F:24:4A:34", Map.ofEntries( // The mac address is from 670_MadMax
                     entry("BACK_RIGHT_MODULE_STEER_OFFSET", -Math.toRadians(292.5)),
+            entry("BACK_LEFT_MODULE_STEER_OFFSET",-Math.toRadians(232.91)),
+            entry("FRONT_RIGHT_MODULE_STEER_OFFSET", -Math.toRadians(352.35)),
+            entry("FRONT_LEFT_MODULE_STEER_OFFSET", -Math.toRadians(136.67))    
+        )),
+        entry("00:80:2F:22:B4:F6", Map.ofEntries( //The mac address is from 670_WCD (test bench)
+            entry("BACK_RIGHT_MODULE_STEER_OFFSET", -Math.toRadians(292.5)),
                     entry("BACK_LEFT_MODULE_STEER_OFFSET", -Math.toRadians(232.91)),
                     entry("FRONT_RIGHT_MODULE_STEER_OFFSET", -Math.toRadians(352.35)),
                     entry("FRONT_LEFT_MODULE_STEER_OFFSET", -Math.toRadians(136.67)),
