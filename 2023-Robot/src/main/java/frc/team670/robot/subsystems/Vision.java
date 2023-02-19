f32e653a9d1be24b2b5f2008f612dbc3e21f8d0e
Ethan <ethanytchang@gmail.com>
2023-02-19 14:55:30 -0800

    vision conflicts fixed

--- 2023-Robot/src/main/java/frc/team670/robot/subsystems/Vision.java
+++ 2023-Robot/src/main/java/frc/team670/robot/subsystems/Vision.java
@@ -1,54 +1,27 @@
 package frc.team670.robot.subsystems;
 
-import org.photonvision.targeting.PhotonPipelineResult;
-import org.photonvision.targeting.PhotonTrackedTarget;
-import edu.wpi.first.math.geometry.Pose2d;
-import edu.wpi.first.math.geometry.Rotation2d;
-import edu.wpi.first.math.geometry.Rotation3d;
-import edu.wpi.first.math.geometry.Transform2d;
+import java.util.Arrays;
+import org.photonvision.PhotonCamera;
+import edu.wpi.first.apriltag.AprilTagFieldLayout;
 import edu.wpi.first.math.geometry.Transform3d;
-import edu.wpi.first.math.geometry.Translation2d;
 import edu.wpi.first.wpilibj.PowerDistribution;
 import frc.team670.mustanglib.subsystems.VisionSubsystemBase;
-import frc.team670.robot.commands.vision.IsLockedOn;
+import frc.team670.robot.constants.FieldConstants;
 import frc.team670.robot.constants.RobotConstants;
 
 public class Vision extends VisionSubsystemBase {
 
     public Vision(PowerDistribution pd) {
-        super(pd);
+        super(pd, new AprilTagFieldLayout(Arrays.asList(FieldConstants.APRILTAGS), FieldConstants.LENGTH, FieldConstants.WIDTH),
+                new PhotonCamera[] {new PhotonCamera(RobotConstants.VISION_CAMERA_NAME)},
+                new Transform3d[] {RobotConstants.CAMERA_OFFSET});
         setName("Vision");
-        // setCamera(RobotConstants.VISION_CAMERA_NAME);
-        setCamera("Arducam_B");
-        super.getCamera().setDriverMode(true);
+        getCameras()[0].setDriverMode(true);
     }
 
     @Override
-    public void mustangPeriodic() {
-        // TODO Auto-generated method stub
-        // super.processImage(0, 0, 0); //TODO: find camera offsets
-    }
+    public void mustangPeriodic() {}
 
     @Override
-    public void debugSubsystem() {
-        // TODO Auto-generated method stub
-
-    }
-
-    // public Transform2d getTransformationToBestTarget() {
-    //     PhotonPipelineResult result = camera.getLatestResult();
-    //     if (result.hasTargets()) {
-    //         PhotonTrackedTarget target = result.getBestTarget();
-    //         Transform3d cameraToTarget = target.getBestCameraToTarget();
-    //         cameraToTarget.getTranslation().toTranslation2d();
-    //         Rotation3d rotation = cameraToTarget.getRotation();
-    //         double x = cameraToTarget.getX();
-    //         double y = cameraToTarget.getY();
-
-            
-            
-    //         return new Transform2d(new Translation2d(x, y), rotation);
-    //     } else return new Transform2d();
-    // }
-
+    public void debugSubsystem() {}
 }
