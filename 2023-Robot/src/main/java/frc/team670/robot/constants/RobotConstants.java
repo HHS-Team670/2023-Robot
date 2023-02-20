bbf79f6e6341681ac58bcaa25f2514e17f6df4cc
Ethan <ethanytchang@gmail.com>
2023-02-19 16:16:37 -0800

    merge errors

--- 2023-Robot/src/main/java/frc/team670/robot/constants/RobotConstants.java
+++ 2023-Robot/src/main/java/frc/team670/robot/constants/RobotConstants.java
@@ -22,6 +22,7 @@ import edu.wpi.first.math.geometry.Transform2d;
 import edu.wpi.first.math.geometry.Transform3d;
 import edu.wpi.first.math.geometry.Transform3d;
 import edu.wpi.first.math.geometry.Translation2d;
+import edu.wpi.first.math.geometry.Translation3d;
 import edu.wpi.first.math.kinematics.DifferentialDriveKinematics;
 import edu.wpi.first.math.trajectory.constraint.DifferentialDriveKinematicsConstraint;
 import edu.wpi.first.wpilibj.SerialPort;
