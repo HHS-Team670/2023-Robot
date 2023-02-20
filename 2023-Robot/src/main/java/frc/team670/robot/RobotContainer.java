08c1ae3c6c0a1061f8cbc65ab20bebd4b9e7f593
Ethan <ethanytchang@gmail.com>
2023-02-19 16:30:30 -0800

    merging artifacts: added missing subsystems

--- 2023-Robot/src/main/java/frc/team670/robot/RobotContainer.java
+++ 2023-Robot/src/main/java/frc/team670/robot/RobotContainer.java
@@ -38,7 +38,9 @@ public class RobotContainer extends RobotContainerBase {
     private final PowerDistribution pd = new PowerDistribution(1, ModuleType.kCTRE);
     
     private final Vision vision = new Vision(pd);
-
+    private final DriveBase driveBase = new DriveBase(getDriverController(), vision);
+    private final Arm arm = new Arm();
+    private final Claw claw = new Claw();
     private static OI oi = new OI();
     
     private Notifier updateArbitraryFeedForwards;
