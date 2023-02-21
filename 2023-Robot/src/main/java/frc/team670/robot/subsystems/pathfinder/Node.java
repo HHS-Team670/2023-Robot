91f85f39a2e30febc7da2cbc6a95fafac932f64f
Ethan <ethanytchang@gmail.com>
2023-02-21 00:48:27 -0800

    added pathfinder files for obstacle avoidance

---
+++ 2023-Robot/src/main/java/frc/team670/robot/subsystems/pathfinder/Node.java
@@ -0,0 +1,58 @@
+package frc.team670.robot.subsystems.pathfinder;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.math.geometry.Translation2d;
+import frc.team670.mustanglib.subsystems.drivebase.SwerveDrive;
+
+public class Node {
+    double x, y;
+    Rotation2d holonomicRotation;
+    List < Node > neighbors;
+  
+    public Node(double x, double y) {
+        this.x = x;
+        this.y = y;
+        holonomicRotation = Rotation2d.fromDegrees(0);
+        this.neighbors = new ArrayList < > ();
+    }
+  
+    public Node(double x, double y, Rotation2d holonomicRotation) {
+        this.x = x;
+        this.y = y;
+        this.holonomicRotation = holonomicRotation;
+        this.neighbors = new ArrayList < > ();
+    }
+
+    public Node(SwerveDrive p){
+      this.x = p.getPose().getX();
+      this.y = p.getPose().getY(); 
+      this.holonomicRotation = p.getPose().getRotation();
+      this.neighbors = new ArrayList < > ();
+    }
+  
+    public Node(Translation2d coordinates, Rotation2d holonomicRotation) {
+      this.x = coordinates.getX();
+      this.y = coordinates.getY(); 
+      this.holonomicRotation = holonomicRotation;
+      this.neighbors = new ArrayList < > ();
+    }
+
+    public void addNeighbor(Node neighbor) {
+        this.neighbors.add(neighbor);
+    }
+    public double getX(){
+      return x;
+    }
+    public double getY(){
+      return y;
+    }
+    public Rotation2d getHolRot(){
+      return holonomicRotation;
+    }
+    public void setHolRot(double degree){
+      this.holonomicRotation = Rotation2d.fromDegrees(degree);
+    } 
+  }
