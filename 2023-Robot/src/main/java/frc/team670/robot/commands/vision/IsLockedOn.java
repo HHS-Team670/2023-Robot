9ebcd30edbe450db190d1d4c2618e033174e2eb9
Tarini Maram <56371094+tarinimaram@users.noreply.github.com>
2023-02-20 22:35:27 -0800

    IsLockedOn checks with a given pose

--- 2023-Robot/src/main/java/frc/team670/robot/commands/vision/IsLockedOn.java
+++ 2023-Robot/src/main/java/frc/team670/robot/commands/vision/IsLockedOn.java
@@ -4,8 +4,11 @@ import java.util.Map;
 
 import javax.print.attribute.HashAttributeSet;
 
+import org.photonvision.targeting.PhotonPipelineResult;
+
 import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.math.geometry.Transform2d;
+import edu.wpi.first.math.geometry.Transform3d;
 import edu.wpi.first.wpilibj2.command.CommandBase;
 import frc.team670.mustanglib.commands.MustangCommand;
 import frc.team670.mustanglib.subsystems.MustangSubsystemBase;
@@ -31,6 +34,13 @@ public class IsLockedOn extends CommandBase implements MustangCommand {
         addRequirements(driveBase, vision);
     }
 
+    public IsLockedOn(SwerveDrive driveBase, VisionSubsystemBase vision, Pose2d goalPose) {
+        this.driveBase = driveBase;
+        this.vision = vision;
+        addRequirements(driveBase, vision);
+        this.goalPose = goalPose;
+    }
+
     @Override
     public Map<MustangSubsystemBase, HealthState> getHealthRequirements() {
         return null;
@@ -44,19 +54,24 @@ public class IsLockedOn extends CommandBase implements MustangCommand {
     @Override
     public void execute() {
         SmartDashboard.putBoolean("isLockedOn", false);
-
         // find pose of nearest target
         currentPose = driveBase.getPose();
-        var result = vision.getCamera().getLatestResult();
+        PhotonPipelineResult result = vision.getCameras()[0].getLatestResult(); 
         SmartDashboard.putBoolean("has targets", result.hasTargets());
         if (!result.hasTargets()) return;
+
+        Pose2d targetPose;
         
-    
-        var camToTarget = result.getBestTarget().getBestCameraToTarget();
-        Transform2d transform = new Transform2d(camToTarget.getTranslation().toTranslation2d(),
+        if (goalPose == null) {
+            Transform3d camToTarget = result.getBestTarget().getBestCameraToTarget();
+            Transform2d transform = new Transform2d(camToTarget.getTranslation().toTranslation2d(),
                 camToTarget.getRotation().toRotation2d());
-        Pose2d cameraPose = currentPose.transformBy(RobotConstants.CAMERA_OFFSET.inverse());
-        Pose2d targetPose = cameraPose.transformBy(transform);
+            Pose2d cameraPose = currentPose.transformBy(new Transform2d(RobotConstants.CAMERA_OFFSET.inverse().getTranslation().toTranslation2d(), 
+                RobotConstants.CAMERA_OFFSET.inverse().getRotation().toRotation2d()));
+            targetPose = cameraPose.transformBy(transform);
+        } else {
+            targetPose = goalPose;
+        }
     
         // transform by offset (to not crash)
         goalPose = targetPose.transformBy(RobotConstants.GRID_TO_TARGET_OFFSET);
