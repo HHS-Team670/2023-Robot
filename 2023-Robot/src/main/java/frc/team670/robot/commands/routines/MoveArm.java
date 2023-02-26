b78884fda4dc41fe470b82f6ffaf8c23d2ff03dc
arghunter <acgo8888@gmail.com>
2023-02-26 14:26:31 -0800

    Update MoveArm.java

--- 2023-Robot/src/main/java/frc/team670/robot/commands/routines/MoveArm.java
+++ 2023-Robot/src/main/java/frc/team670/robot/commands/routines/MoveArm.java
@@ -4,22 +4,25 @@ import java.util.HashMap;
 import java.util.Map;
 
 import edu.wpi.first.wpilibj2.command.ParallelCommandGroup;
+import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 import frc.team670.mustanglib.commands.MustangCommand;
 import frc.team670.mustanglib.subsystems.MustangSubsystemBase;
 import frc.team670.mustanglib.subsystems.MustangSubsystemBase.HealthState;
 import frc.team670.robot.commands.arm.MoveToTarget;
+import frc.team670.robot.commands.claw.ClawIntake;
 import frc.team670.robot.subsystems.Claw;
 import frc.team670.robot.subsystems.Claw.Status;
 import frc.team670.robot.subsystems.arm.Arm;
 import frc.team670.robot.subsystems.arm.ArmState;
 
-public class MoveArm extends ParallelCommandGroup implements MustangCommand {
+public class MoveArm extends SequentialCommandGroup implements MustangCommand {
     private Map<MustangSubsystemBase, HealthState> healthReqs;
 
     public MoveArm(Arm arm, Claw claw, ArmState target) {
         healthReqs = new HashMap<MustangSubsystemBase, HealthState>();
         healthReqs.put(arm, HealthState.GREEN);
         healthReqs.put(claw, HealthState.GREEN);
+        addCommands(new ClawIntake(claw, false));
         addCommands(new MoveToTarget(arm, target));
         addCommands(new ClawToggle(claw, arm, Status.INTAKING));
     }
