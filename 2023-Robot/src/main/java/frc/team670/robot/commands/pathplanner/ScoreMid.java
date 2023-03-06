19d0473af4ff2ffd71d5ebffeae60ede84674c29
Tarini Maram <56371094+tarinimaram@users.noreply.github.com>
2023-03-06 13:02:30 -0800

    auto command that just scores and sits

---
+++ 2023-Robot/src/main/java/frc/team670/robot/commands/pathplanner/ScoreMid.java
@@ -0,0 +1,28 @@
+package frc.team670.robot.commands.pathplanner;
+
+import java.util.HashMap;
+import java.util.Map;
+import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
+import frc.team670.mustanglib.commands.MustangCommand;
+import frc.team670.mustanglib.subsystems.MustangSubsystemBase;
+import frc.team670.mustanglib.subsystems.MustangSubsystemBase.HealthState;
+import frc.team670.robot.commands.arm.MoveToTarget;
+import frc.team670.robot.commands.claw.ClawEject;
+import frc.team670.robot.subsystems.Claw;
+import frc.team670.robot.subsystems.DriveBase;
+import frc.team670.robot.subsystems.arm.Arm;
+import frc.team670.robot.subsystems.arm.ArmState;
+
+public class ScoreMid extends SequentialCommandGroup implements MustangCommand {
+
+    public Map<MustangSubsystemBase, HealthState> getHealthRequirements() {
+        return new HashMap<MustangSubsystemBase, HealthState>();
+    }
+
+    public ScoreMid(DriveBase driveBase, Claw claw, Arm arm) {
+        addCommands(new MoveToTarget(arm, ArmState.SCORE_MID), 
+                    new ClawEject(claw), 
+                    new MoveToTarget(arm, ArmState.STOWED));
+    }
+
+}
