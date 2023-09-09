234465973467d23cd9a2665469e48e4f4d4d66f7
Aditi Kattuparambil <112968168+KattuparambilAditi@users.noreply.github.com>
2023-09-09 11:59:38 -0700

    mid and high

--- 2023-Robot/src/main/java/frc/team670/robot/subsystems/arm/ArmState.java
+++ 2023-Robot/src/main/java/frc/team670/robot/subsystems/arm/ArmState.java
@@ -15,9 +15,9 @@ public enum ArmState {
 
     HYBRID(1, 140, 50, 254),
 
-    SCORE_MID(2, 199, 58, 179), 
+    SCORE_MID(2, 199, 71, 179), 
     // SCORE_MID(2, 191, 91, 194),  possible score high
-    SCORE_HIGH(3, 165, 109, 158),
+    SCORE_HIGH(3, 165, 119, 180),
     STARTING(4, 205,25,270),
     TUNING(5, 90, 180, 180),
     SINGLE_STATION(6, 231, 13, 249),
