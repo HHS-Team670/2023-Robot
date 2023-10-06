49daf2b50a8028c64833b47f940a999791aa045b
Armaan Gomes <91099806+arghunter@users.noreply.github.com>
2023-10-05 17:32:00 -0700

    camera stream toggles

--- Dashboard2023/ui.js
+++ Dashboard2023/ui.js
@@ -47,20 +47,20 @@ function setPaths() {
 setPaths()
 
 
-// var toggleCamera = document.querySelector('#toggle-camera')
-// toggleCamera.onclick = () => {
-//     var cameraDiv = document.querySelector("#camera-streams")
-//     if (document.querySelector(".camera-stream") == null) {
-//         var cameraStreams = `<image class="camera-stream" src="http://photonvision.local:1182/stream.mjpg?1664842058013"></image>
-//         <image class="camera-stream" src="http://10.6.70.2:1181"></image>`
-//         // var cameraStreams = `<image class="camera-stream" src="http://photonvision.local:1184/stream.mjpg?1646964457545"></image>`
-//         cameraDiv.insertAdjacentHTML( 'beforeend', cameraStreams )
-//     } else {
-//         while (cameraDiv.firstChild) {
-//             cameraDiv.removeChild(cameraDiv.firstChild)
-//         }
-//     }
-// }
+var toggleCamera = document.querySelector('#toggle-camera');
+toggleCamera.onclick = () => {
+    var cameraDiv = document.querySelector("#camera-streams")
+    if (document.querySelector(".camera-stream") == null) {
+        var cameraStreams = `</image>
+        <image class="camera-stream" src="http://10.6.70.2:1181"></image>`
+        // var cameraStreams = `<image class="camera-stream" src="http://photonvision.local:1184/stream.mjpg?1646964457545"></image>`;
+        cameraDiv.insertAdjacentHTML( 'beforeend', cameraStreams );
+    } else {
+        while (cameraDiv.firstChild) {
+            cameraDiv.removeChild(cameraDiv.firstChild);
+        }
+    }
+}
 
 // // listens for robot-state and updates status lights and auton chooser accordingly
 // NetworkTables.addKeyListener('/SmartDashboard/robot-state', (key, value) => {
@@ -169,28 +169,10 @@ NetworkTables.addKeyListener('/SmartDashboard/match-started', (key, value) => {
 //     }
 // })
 
-NetworkTables.addKeyListener('/SmartDashboard/Single Substation', (key, poseString) => {
-    single_substation_coords = parsePose(poseString)
-})
-var video = document.getElementById("video");
-var source = new XMLHttpRequest();
-source.open("GET", "http://photonvision.local:1181/stream.mjpg", true);
-source.responseType = "arraybuffer";
-source.onload = function() {
-  var buffer = source.response;
-  var videoCtx = video.getContext("2d");
-  var ctx = videoCtx.getImageData(0, 0, videoCtx.canvas.width, videoCtx.canvas.height);
-  for (var i = 0; i < ctx.data.length; i += 4) {
-    var red = (ctx.data[i] << 16) | (ctx.data[i + 1] << 8) | ctx.data[i];
-    var green = (ctx.data[i + 1] << 16) | (ctx.data[i + 2] << 8) | ctx.data[i + 3];
-    var blue = (ctx.data[i + 2] << 16) | (ctx.data[i + 3] << 8) | ctx.data[i + 4];
-    ctx.data[i] = red;
-    ctx.data[i + 1] = green;
-    ctx.data[i + 2] = blue;
-  }
-  videoCtx.putImageData(ctx, 0, 0);
-};
-source.send();
+// NetworkTables.addKeyListener('/SmartDashboard/Single Substation', (key, poseString) => {
+//     single_substation_coords = parsePose(poseString)
+// })
+
 // NetworkTables.addKeyListener('/SmartDashboard/Estimated Pose', (key, poseString) => {
 //     var field_coord = parsePose(poseString)
 //     var relX = field_coord[0] - single_substation_coords[0]
@@ -204,12 +186,12 @@ source.send();
 
 // })
 
-var parsePose = (poseString) => {
-    var x = Number(poseString.substring(1, poseString.indexOf(",")))
-    var y = Number(poseString.substring(poseString.indexOf(",") + 2, poseString.indexOf(")")))
-    var rot = Number(poseString.substring(poseString.indexOf(")") + 2, poseString.indexOf("degrees")))
-    return [x, y, rot]
-}
+// var parsePose = (poseString) => {
+//     var x = Number(poseString.substring(1, poseString.indexOf(",")))
+//     var y = Number(poseString.substring(poseString.indexOf(",") + 2, poseString.indexOf(")")))
+//     var rot = Number(poseString.substring(poseString.indexOf(")") + 2, poseString.indexOf("degrees")))
+//     return [x, y, rot]
+// }
 NetworkTables.addKeyListener('/SmartDashboard/Shoulder position (deg)', (key, value) => {
     var shoulderValue = document.querySelector("h3#shoulder-value")
     shoulderValue.innerHTML = "Shoulder Position (deg): " + Number(value).toFixed(2)
